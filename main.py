559ca94c4b02f2c38abadef195f02746151cd81b
farisKomo <farisedu9@gmail.com>
2023-11-11 16:57:11 +0300

    A lot of changes

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-from flask import Flask, render_template, request
+from website import create_app
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/', methods=['GET', 'POST'])
-def upload_pdf():
-    if request.method == 'POST':
-        pdf_file = request.files['pdfFile']
-        if pdf_file and pdf_file.filename.endswith('.pdf'):
-            pdf_file.save('uploads/' + pdf_file.filename)
-            return 'File uploaded successfully.'
-        else:
-            return 'Please select a valid PDF file.'
-    return render_template('index.html')
-
+app = create_app()
+app.secret_key = "ff11223344"
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ website/view.py
@@ -0,0 +1,94 @@
+from flask import render_template, Blueprint, request, session, redirect, url_for
+from website import generatingQuestions
+import json
+
+view = Blueprint('view', __name__)
+
+test = [{"id": 2,
+                "question": "who is the king of football in 2012?",
+                "options": [
+                    "Cristiano Ronaldo",
+                    "Malek Moath",
+                    "Faris Alshumrani",
+                    "Lionel Messi"
+                ],
+                "correct_answer": "Lionel Messi"
+            },
+            {"id": 3,
+                "question": "How many people in the earth?",
+                "options": [
+                    "7.888 billions",
+                    "2 billions",
+                    "3 billions",
+                    "15 billions"
+                ],
+                "correct_answer": "7.888 billions"
+            }
+
+            ]
+
+@view.route('/')
+def upload_pdf():
+    return render_template('index.html')
+
+@view.route('/', methods = ['GET', 'POST'])
+def upload_file():
+
+   if request.method == 'POST':
+      if 'file' in request.files:
+        f = request.files['file']
+        questionsCount = request.form.get('questionCount')
+
+        text = generatingQuestions.loadPdf(f)
+        gptR = generatingQuestions.get_response(text, questionsCount)["questions"]
+        session['gptR'] = gptR
+        session['currentQuestion'] = 0
+        session['score'] = 0
+        session['incorrect_answers'] = []
+        session['total'] = len(gptR)
+        print(gptR)
+
+        return render_template('index.html', data = gptR[0])
+      elif 'quiz-option' in request.form:
+        selected_option = request.form.get('quiz-option')
+        gptR = session.get('gptR')
+        currentQuestion = session.get('currentQuestion')
+
+        correct_answer = gptR[currentQuestion]["correct_answer"]
+        if selected_option == correct_answer:
+          print("correct")
+          session['score'] += 1
+        else:
+          print("false")
+          session['incorrect_answers'].append({
+              'question':gptR[currentQuestion]["question"],
+              'your_answer':selected_option,
+              'correct_answer':correct_answer
+          })
+          
+        if currentQuestion + 1 < len(gptR):
+          session['currentQuestion'] += 1
+
+          return redirect(url_for('view.display_question'))
+        else:
+          score = session.get('score')
+          total = session.get('total')
+          incorrect_answers = session.get('incorrect_answers')
+          return render_template('index.html', myScore = f"{score}/{total}", incorrect_answers=incorrect_answers)
+
+      else:
+         return render_template('index.html')
+
+  
+
+@view.route('/display_question')
+def display_question():
+    gptR = session.get('gptR')
+    currentQuestion = session.get('currentQuestion')
+
+    return render_template('index.html', data=gptR[currentQuestion])
+
+
+
+
+
