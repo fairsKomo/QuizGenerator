023920996ef50c740a9cdf727ac6ee83e787cd8c
farisKomo <farisedu9@gmail.com>
2023-11-21 18:33:02 +0300

    Revert "Procfile Modified"
    This reverts commit f06b259f8dc86cbd28a2c56e9da605beeafe6945.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from website import create_app
-import os
 
 app = create_app()
 app.secret_key = "ff11223344"
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
