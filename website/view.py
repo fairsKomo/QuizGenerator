5320e5d974ec0d1a3588269d4738ad44dc648be9
farisKomo <farisedu9@gmail.com>
2023-11-21 19:19:35 +0300

    To be deployed test

--- website/view.py
+++ website/view.py
@@ -1,12 +1,8 @@
 from flask import render_template, Blueprint, request, session, redirect, url_for
 from website import generatingQuestions
-from redis import Redis
-from rq import Worker, Queue, Connection
+import json
 
 view = Blueprint('view', __name__)
-redis_conn = Redis.from_url('redis://localhost:6379/0')
-rq_queue = Queue('default', connection=redis_conn)
-
 
 @view.route('/')
 def upload_pdf():
@@ -21,17 +17,15 @@ def upload_file():
         questionsCount = request.form.get('questionCount')
 
         text = generatingQuestions.loadPdf(f)
-        job = rq_queue.enqueue(generatingQuestions.get_response, text, questionsCount)
-        session['job_id'] = job.get_id()
+        gptR = generatingQuestions.get_response(text, questionsCount)["questions"]
+        session['gptR'] = gptR
+        session['currentQuestion'] = 0
+        session['score'] = 0
+        session['incorrect_answers'] = []
+        session['total'] = len(gptR)
+        print(gptR)
 
-        return render_template('processing.html')
-      
-        # session['score'] = 0
-        # session['incorrect_answers'] = []
-        # session['total'] = len(gptR)
-        # print(gptR)
-
-        # return render_template('index.html', data = gptR[0])
+        return render_template('index.html', data = gptR[0])
       elif 'quiz-option' in request.form:
         selected_option = request.form.get('quiz-option')
         gptR = session.get('gptR')
@@ -66,24 +60,7 @@ def upload_file():
 
 @view.route('/display_question')
 def display_question():
-  job_id = session.get('job_id')
+    gptR = session.get('gptR')
+    currentQuestion = session.get('currentQuestion')
 
-  if job_id:
-      job = rq_queue.fetch_job(job_id)
-
-      if job.is_finished:
-          gptR = job.result["questions"]
-          session.pop('job_id', None)
-      else:
-          return render_template('processing.html')
-
-  else:
-      gptR = session.get('gptR')
-
-  currentQuestion = session.get('currentQuestion')
-  return render_template('index.html', data=gptR[currentQuestion])
-
-
-
-
-
+    return render_template('index.html', data=gptR[currentQuestion])
