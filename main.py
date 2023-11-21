59be00f619daaeec87e8edb8b882237add39d1e7
farisKomo <farisedu9@gmail.com>
2023-11-22 00:51:24 +0300

    try

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ import os
 app = create_app()
 app.secret_key = "ff11223344"
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
